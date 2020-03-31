af4052914a4fb2f0d6a4c2149dae5225675c86a8
AshitaGK <ashita.gk696@gmail.com>
2020-03-31 14:55:47 +0530

    Updates

---
+++ precache-manifest.6559404fa31165354a41b9e618e09fcc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a419e6cc4937462b0043eb434b0f0f82",
+    "url": "/NoteApp/index.html"
+  },
+  {
+    "revision": "bae6bee2a70d0a531f24",
+    "url": "/NoteApp/static/js/2.38c3dfdb.chunk.js"
+  },
+  {
+    "revision": "c2def67314e9d7f2aaca",
+    "url": "/NoteApp/static/js/main.2836d547.chunk.js"
+  },
+  {
+    "revision": "f88a4ea75060304d0f24",
+    "url": "/NoteApp/static/js/runtime-main.7905d236.js"
+  }
+]);
